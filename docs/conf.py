535d61096601b8051ba049521c6ef9243fd49575
Daan Renders <daan@fluves.com>
2023-10-10 16:00:39 +0200

    Revert "Add nbsphinx for docs"
    This reverts commit d087b54c67a46ef9a07f12f44a1e62f67c0eee37.

--- docs/conf.py
+++ docs/conf.py
@@ -63,6 +63,8 @@ except Exception as e:
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
+	"myst_parser",
+	"sphinx_copybutton",
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "sphinx.ext.todo",
@@ -73,8 +75,6 @@ extensions = [
     "sphinx.ext.ifconfig",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
-    "sphinx.ext.githubpages",
-    "nbsphinx"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
