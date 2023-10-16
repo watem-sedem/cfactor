e2ba003e43c058657d2aa9ffb26f97e1da3f62a3
Daan Renders <daan@fluves.com>
2023-10-16 16:08:27 +0200

    Add empty testfunctions for every function to test

--- tests/test_cfactor.py
+++ tests/test_cfactor.py
@@ -37,3 +37,45 @@ def test_compute_crop_cover():
     with pytest.raises(ValueError) as excinfo:
         cc = cfactor.compute_crop_cover(H, Fc)
     assert ("Soil cover must be between 0 and 1") in str(excinfo.value)
+
+
+def test_compute_soil_roughness():
+    """Test calculation of soil roughness"""
+    # TO DO
+    # cfactor.compute_soil_roughness()
+
+
+def test_compute_surface_roughness():
+    """Test calculation of surface roughness"""
+    # TO DO
+    # cfactor.compute_surface_roughness()
+
+
+def test_compute_crop_residu():
+    """Test calculation of crop residu"""
+    # TO DO
+    # cfactor.compute_crop_residu()
+
+
+def test_compute_harvest_residu_decay_rate():
+    """Test calculation of harvest residu decay rate"""
+    # TO DO
+    # cfactor.compute_harvest_residu_decay_rate()
+
+
+def test_compute_soil_cover():
+    """Test calculation of soil cover"""
+    # TO DO
+    # cfactor.compute_soil_cover()
+
+
+def test_compute_soil_loss_ratio():
+    """Test calculation of soil loss ration"""
+    # TO DO
+    # cfactor.compute_soil_loss_ratio()
+
+
+def test_aggregate_slr_to_crop_factor():
+    """Test aggregation of SLR to C-factor"""
+    # TO DO
+    # cfactor.aggregate_slr_to_c_factor()
