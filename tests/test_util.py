76cab244ead15e6d1f281c88cec2973411cee2e9
Daan Renders <daan@fluves.com>
2024-01-02 11:22:19 +0100

    Switch to np testing in stead of pandas testing

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import numpy as np
 
 from cfactor import util
 
@@ -12,11 +12,12 @@ def test_celc_to_fahr():
     temperature_fahrenheit = util.celc_to_fahr(temperature_celcius)
     assert expected_temperature == temperature_fahrenheit
 
-    # Test with pandas series
-    temperature_celcius_series = pd.Series(data=[0, 10, 24, -10])
-    expected_temperature_series = pd.Series(data=[32.0, 50.0, 75.2, 14.0])
+    # Test with numpy array
+    temperature_celcius_series = np.array([0, 10, 24, -10])
+
+    expected_temperature_series = np.array([32.0, 50.0, 75.2, 14.0])
 
     temperature_fahrenheit_series = util.celc_to_fahr(temperature_celcius_series)
-    pd.testing.assert_series_equal(
+    np.testing.assert_allclose(
         expected_temperature_series, temperature_fahrenheit_series
     )
